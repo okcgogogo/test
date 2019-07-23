8388137e5edd0cec022c75f403e2dc1d25a2edfa
Charlie <xiangli0602@126.com>
2019-07-23 08:24:48 +0800

    阅读数累加1

--- src/main/java/com/charlie/test/controller/QuestionController.java
+++ src/main/java/com/charlie/test/controller/QuestionController.java
@@ -17,6 +17,7 @@ public class QuestionController {
     @GetMapping("/question/{id}")
     public String question(@PathVariable("id") Integer id, Model model){
         QuestionDTO questionDTO = questionService.getById(id);
+        // 阅读数累加
         questionService.incrview(id);
         model.addAttribute("question",questionDTO);
         return "question";
